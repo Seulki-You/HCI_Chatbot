29213db44ca0df2a548c957366781d0bb5c598fc
seulki <dworker96@gmail.com>
2017-12-10 13:55:05 +0900

    DB

--- SSUperBot/settings.py
+++ SSUperBot/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'SSUperBot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lecture',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': 'dotnf369!@',
     }
 }
 
